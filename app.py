b09b072c2b75036f35e74de19bc11c6fb2676c21
qiao-blip <1719598381@qq.com>
2026-06-12 23:57:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+import redis
+import os
 
+app = Flask(__name__)
+
+rd = redis.Redis(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True
+)
+
+@app.route("/api/ping")
+def ping():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
